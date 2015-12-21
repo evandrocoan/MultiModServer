44d19a9971c62eb78ac802dcf4e4755db5168cdc
evandrocoan <evandrocoan@hotmail.com>
2015-12-20 22:37:09 -0200

    Replaced amxx plugins by its equivalent amxx list at galileo_reloaded-bbcode.txt

--- scripting/Compile_and_Install.bat
+++ scripting/Compile_and_Install.bat
@@ -1,3 +1,4 @@
 compile.exe
+xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\cstrike\addons\amxmodx\plugins"
 xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\czero\addons\amxmodx\plugins"
 pause
