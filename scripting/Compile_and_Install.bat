16fc81d243842f181e72a6314b6b080cb7b7b6b8
evandrocoan <evandrocoan@hotmail.com>
2015-12-22 07:26:35 -0200

    Created a full general command line batch to compile any project.

--- scripting/Compile_and_Install.bat
+++ scripting/Compile_and_Install.bat
@@ -1,9 +1,29 @@
-compile.exe
+@echo off
 
-xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\czero\addons\amxmodx\plugins"
-xcopy /E /S /Y ".\..\data" "F:\SteamCMD\steamapps\common\Half-Life\czero\addons\amxmodx\data"
+for %%i in (*.sma) do (
+    echo.
+    echo uncrustify.exe -c uncrustify\cfg\amxmodx.cfg --no-backup %%i
+    uncrustify.exe -c uncrustify\cfg\amxmodx.cfg --no-backup %%i
+    
+    echo intend_empty_lines.lua %%i > temp_file.txt
+    intend_empty_lines.lua %%i > temp_file.txt
+    del %%i
+
+    :: convert the EOF from CRLF to LF due the loss by the " > " above. 
+    echo tr -d '\r' < temp_file.txt > %%i
+    tr -d '\r' < temp_file.txt > %%i
+    del temp_file.txt
+
+    echo.
+    echo // Compiling %%i ...
+    echo.
+
+    amxxpc.exe "%%i" -ocompiled/"%%~ni.amxx"
+
+    echo.
+)
 
 xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\cstrike\addons\amxmodx\plugins"
-xcopy /E /S /Y ".\..\data" "F:\SteamCMD\steamapps\common\Half-Life\cstrike\addons\amxmodx\data"
+xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\czero\addons\amxmodx\plugins"
 
-pause
+if "%1"=="" pause
