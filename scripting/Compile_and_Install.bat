43924c444a91e5e85d82ba89d604849291ff177d
addonszz <addons_zz@outlook.com>
2015-12-22 05:53:14 -0200

    Added a lua script to insert intent spaces in blank lines.

--- scripting/Compile_and_Install.bat
+++ scripting/Compile_and_Install.bat
@@ -1,4 +1,7 @@
-uncrustify.exe -c uncrustify\cfg\amxmodx.cfg -f galileo_reloaded.sma -o galileo_reloaded.sma
+uncrustify.exe -c uncrustify\cfg\amxmodx.cfg --no-backup galileo_reloaded.sma
+
+intend_empty_lines.lua galileo_reloaded.sma > temp_file.txt
+move /Y temp_file.txt galileo_reloaded.sma
 
 compile.exe
 xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\cstrike\addons\amxmodx\plugins"
