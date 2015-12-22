1a544adb321f773da0d96e863f5d63ab4ce4190d
addonszz <addons_zz@outlook.com>
2015-12-22 06:07:44 -0200

    Fixed lf EOF replacemente by crlf EOF.

--- scripting/Compile_and_Install.bat
+++ scripting/Compile_and_Install.bat
@@ -1,7 +1,11 @@
 uncrustify.exe -c uncrustify\cfg\amxmodx.cfg --no-backup galileo_reloaded.sma
 
 intend_empty_lines.lua galileo_reloaded.sma > temp_file.txt
-move /Y temp_file.txt galileo_reloaded.sma
+del galileo_reloaded.sma
+
+rem convert the EOF from CRLF to LF due the loss by the " > " above. 
+tr -d '\r' < temp_file.txt > galileo_reloaded.sma
+del temp_file.txt
 
 compile.exe
 xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\cstrike\addons\amxmodx\plugins"
