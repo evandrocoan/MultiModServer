932ed0aa793d9d604d400bd424cfd23376fd066a
evandrocoan <evandrocoan@hotmail.com>
2015-12-22 07:20:17 -0200

    Created a full general command line batch to compile any project.

--- scripting/Compile_and_Install.bat
+++ scripting/Compile_and_Install.bat
@@ -1,14 +1,29 @@
-uncrustify.exe -c uncrustify\cfg\amxmodx.cfg --no-backup galileo_reloaded.sma
+@echo off
 
-intend_empty_lines.lua galileo_reloaded.sma > temp_file.txt
-del galileo_reloaded.sma
+for %%i in (*.sma) do (
+    echo.
+    echo uncrustify.exe -c uncrustify\cfg\amxmodx.cfg --no-backup %%i
+    uncrustify.exe -c uncrustify\cfg\amxmodx.cfg --no-backup %%i
+    
+    echo intend_empty_lines.lua %%i > temp_file.txt
+    intend_empty_lines.lua %%i > temp_file.txt
+    del %%i
 
-rem convert the EOF from CRLF to LF due the loss by the " > " above. 
-tr -d '\r' < temp_file.txt > galileo_reloaded.sma
-del temp_file.txt
+    :: convert the EOF from CRLF to LF due the loss by the " > " above. 
+    echo tr -d '\r' < temp_file.txt > %%i
+    tr -d '\r' < temp_file.txt > %%i
+    del temp_file.txt
 
-compile.exe
+    echo.
+    echo // Compiling %%i ...
+    echo.
+
+    amxxpc.exe "%%i" -ocompiled/"%%~ni.amxx"
+
+    echo.
+)
+
 xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\cstrike\addons\amxmodx\plugins"
 xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\czero\addons\amxmodx\plugins"
 
-pause
+if "%1"=="" pause
