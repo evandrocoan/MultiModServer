91c16ba0088d945034191e0776c77c7f6292c48f
addonszz <addons_zz@outlook.com>
2015-12-23 03:23:49 -0200

    Separeted the compile batch file from the formatter batch file.

--- scripting/Compile_and_Install.bat
+++ scripting/Compile_and_Install.bat
@@ -2,25 +2,10 @@
 
 for %%i in (*.sma) do (
     echo.
-    echo uncrustify.exe -c amxmodx.cfg --no-backup %%i
-    uncrustify.exe -c amxmodx.cfg --no-backup %%i
-    
-    echo intend_empty_lines.lua %%i > temp_file.txt
-    intend_empty_lines.lua %%i > temp_file.txt
-    del %%i
-
-    :: convert the EOF from CRLF to LF due the loss by the " > " above. 
-    echo tr -d '\r' < temp_file.txt > %%i
-    tr -d '\r' < temp_file.txt > %%i
-    del temp_file.txt
-
-    echo.
     echo // Compiling %%i ...
     echo.
 
     amxxpc.exe "%%i" -ocompiled/"%%~ni.amxx"
-
-    echo.
 )
 
 xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\cstrike\addons\amxmodx\plugins"
