647a84f878c1645194e3f259fb820108f83989ba
evandrocoan <evandrocoan@hotmail.com>
2015-12-26 14:48:04 -0200

    Implemented mp_winlimit and mp_maxrounds end map voting.

--- scripting/Compile_and_Install.bat
+++ scripting/Compile_and_Install.bat
@@ -8,7 +8,13 @@ for %%i in (*.sma) do (
     amxxpc.exe "%%i" -ocompiled/"%%~ni.amxx"
 )
 
+echo.
 xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\cstrike\addons\amxmodx\plugins"
-xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\czero\addons\amxmodx\plugins"
+echo xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\cstrike\addons\amxmodx\plugins"
 
+echo.
+xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\czero\addons\amxmodx\plugins"
+echo xcopy /E /S /Y ".\compiled" "F:\SteamCMD\steamapps\common\Half-Life\czero\addons\amxmodx\plugins"
+
+echo.
 if "%1"=="" pause
