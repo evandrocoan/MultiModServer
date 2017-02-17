e824414f582f21a3ca8f6789e28f44d0fa8f3e34
evandrocoan <evandrocoan@hotmail.com>
2017-02-17 00:24:03 -0200

    Created installers for the multimod server and plugin on installers/

---
+++ installers/install_multimod_server.sh
@@ -0,0 +1,18 @@
+
+
+
+
+rsync -rvu --perms --exclude="**server.cfg" --exclude="**users.ini" \
+"/cygdrive/D/User/Dropbox/Applications/SoftwareVersioning/MultiModServer/dependencies/server_files/gamemod_common/" \
+"/cygdrive/F/SteamCMD/steamapps/common/Half-Life/czero/"
+
+
+rsync -rvu --perms \
+"/cygdrive/D/User/Dropbox/Applications/SoftwareVersioning/MultiModServer/dependencies/server_files/cstrike_czero/" \
+"/cygdrive/F/SteamCMD/steamapps/common/Half-Life/czero/"
+
+
+
+
+
+
